0917b71bef56d5947259e714ae8ace65f8540742
Sololo <philasololo@gmail.com>
2022-11-12 16:59:15 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-import './components/modulesapi.js'
+import './components/podcast-app.js'
+import './components/podcast-view-list.js'
+import './components/podcast-view-single.js'
+import './components/podcast-controls'
